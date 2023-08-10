86e504635005de0af652970e0642f1a9ae33881c
Tomas Rodriguez <tomasrod09@hotmail.com>
2023-08-10 16:23:26 -0700

    clean code and add comments

--- Day 2/solution.js
+++ Day 2/solution.js
@@ -1,6 +1,24 @@
 import * as fs from 'fs/promises'
 
-const getMyScore = async () => {
+/** Part 1
+ * A and X - Rock 1pt
+ * B and Y - Paper 2pts
+ * C and Z - Scissor 3pts
+ */
+
+const scoreMap1 = {
+  'A Y': 8,
+  'B Z': 9,
+  'C X': 7,
+  'A X': 4,
+  'B Y': 5,
+  'C Z': 6,
+  'B X': 1,
+  'C Y': 2,
+  'A Z': 3,
+}
+
+const getMyScore = async (scoreMap) => {
   const getData = async () => {
     try {
       const data = await fs.readFile('./input.txt', {encoding: 'utf-8'} )
@@ -13,24 +31,33 @@ const getMyScore = async () => {
   const inputData = await getData()
   const formattedData = inputData.split('\n')
 
-  const scoreMap = {
-    'A Y': 8,
-    'B Z': 9,
-    'C X': 7,
-    'A X': 4,
-    'B Y': 5,
-    'C Z': 6,
-    'B X': 1,
-    'C Y': 2,
-    'A Z': 3,
-  }
-
   let myScore = 0
   formattedData.forEach((round) => {
    myScore += scoreMap[round] || 0
   })
-  console.log(myScore)
   return myScore
 }
 
-getMyScore()
+console.log('Part 1 score:', await getMyScore(scoreMap1))
+
+/** Part 2
+ * X - I lose
+ * Y - I draw
+ * Z - I win
+ */
+
+const scoreMap2 = {
+  'A X': 3,
+  'A Y': 4,
+  'A Z': 8,
+  'B X': 1,
+  'B Y': 5,
+  'B Z': 9,
+  'C X': 2,
+  'C Y': 6,
+  'C Z': 7,
+}
+
+console.log('Part 2 score:', await getMyScore(scoreMap2))
+
+
