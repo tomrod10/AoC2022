e56f151e11013dbc471e20cfe0d8a88377681ff0
Tomas Rodriguez <tomasrod09@hotmail.com>
2023-08-10 16:05:32 -0700

    clean implementation + finish part 1

--- Day 2/solution.js
+++ Day 2/solution.js
@@ -1,42 +1,36 @@
 import * as fs from 'fs/promises'
 
-const getData = async () => {
-  try {
-    const data = await fs.readFile('./input.txt', {encoding: 'utf-8'} )
-    return data
-  } catch (err) {
-    console.log(`Something went wrong: ${err}`)
+const getMyScore = async () => {
+  const getData = async () => {
+    try {
+      const data = await fs.readFile('./input.txt', {encoding: 'utf-8'} )
+      return data
+    } catch (err) {
+      console.log(`Something went wrong: ${err}`)
+    }
   }
+
+  const inputData = await getData()
+  const formattedData = inputData.split('\n')
+
+  const scoreMap = {
+    'A Y': 8,
+    'B Z': 9,
+    'C X': 7,
+    'A X': 4,
+    'B Y': 5,
+    'C Z': 6,
+    'B X': 1,
+    'C Y': 2,
+    'A Z': 3,
+  }
+
+  let myScore = 0
+  formattedData.forEach((round) => {
+   myScore += scoreMap[round] || 0
+  })
+  console.log(myScore)
+  return myScore
 }
 
-const inputData = await getData()
-let cleanHands = []
-
-for (let i = 0; i < inputData.length; i++) {
-  if (i % 2 === 0) {
-    cleanHands.push(inputData[i])
-  }
-}
-
-const col1 = cleanHands.filter((hand) => {
-  return ['A', 'B', 'C'].includes(hand)
-})
-const col2 = cleanHands.filter((hand) => {
-  return ['X', 'Y', 'Z'].includes(hand)
-})
-
-console.log(col2)
-
-/**
- * column 1 {
- * A : 'Rock',
- * B : 'Paper',
- * C : 'Scissors'
- * }
- *
- * column 2 {
- * X : 'Rock',
- * Y : 'Paper',
- * Z : 'Scissors'
- * }
- */
+getMyScore()
